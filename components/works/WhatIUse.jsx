29bb290ecc44c1fbc4ff6eff5f7a443e87c23fcf
Marlène D <marlene.a.diaz@gmail.com>
2023-08-01 15:07:01 +0200

    Adding height for icon next Images

--- components/works/WhatIUse.jsx
+++ components/works/WhatIUse.jsx
@@ -9,7 +9,7 @@ const WhatIUse = ({elements, categoryName}) => {
             <h2 className='font-sansita font-bold text-3xl p-4 mb-4 ml-3 text-center'>{categoryName} :</h2>
             {elements.map(element => 
                 <div className='group relative flex justify-center'>
-                    <Image src={element.icon} alt={element.alt} key={element.id} height={70} width={70} className='m-2 place-self-center' />
+                    <Image src={element.icon} alt={element.alt} key={element.id} width={70} className='m-2 place-self-center h-auto' />
                     <span className="absolute bottom-0 scale-0 rounded bg-purple-dark p-1 text-[10px] font-roboto text-white group-hover:scale-100">{element.id}</span>
                 </div>
             )} 
