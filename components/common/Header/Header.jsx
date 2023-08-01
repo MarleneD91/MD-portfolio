f77d3a55d654843c1d5ecbde05f6a49f9efca0e3
Marlène D <marlene.a.diaz@gmail.com>
2023-08-01 15:03:05 +0200

    Light change for Logo

--- components/common/Header/Header.jsx
+++ components/common/Header/Header.jsx
@@ -6,8 +6,8 @@ import DarkLogo from '../../../public/images/dark-logo.png'
 
 const Header = () => {
   return (
-    <div className='bg-green-light text-black flex flex-row items-center justify-between p-3'>
-      <a href="/" className="relative"><Image src={DarkLogo} width={100} height={102} alt='Logo of MD, black version, letters and Cymothoe (butterfly) in an arrow circle'/></a>
+    <div className='bg-green-light text-black flex flex-row items-center justify-between p-3 w-auto'>
+      <a href="/" className="relative"><Image src={DarkLogo} height={102} alt='Logo of MD, black version, letters and Cymothoe (butterfly) in an arrow circle' className='h-auto'/></a>
       <Navbar/>
     </div>
   )
