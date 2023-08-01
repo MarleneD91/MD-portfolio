92b144bfb14a51cc1be7f5cd433051f5d2efaf9f
Marlène D <marlene.a.diaz@gmail.com>
2023-08-01 15:04:00 +0200

    Changing Cactus illustration size

--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
 import Header from '@/components/common/Header/Header'
 import Footer from '@/components/common/Footer/Footer'
 
-import HomeAnimatedTitle from '@/components/home/HomeAnimatedText'
+import HomeAnimatedText from '@/components/home/HomeAnimatedText'
 
 import Image from 'next/image'
 import Cactus from '@/public/images/Cactus.png'
-import HomeAnimatedText from '@/components/home/HomeAnimatedText'
 
 export default function Home() {
   return (
@@ -14,8 +13,8 @@ export default function Home() {
         <main >
           <div className='text-black flex flex-col items-center justify-between'>
             <HomeAnimatedText/>
-            <div className='group relative m-12 flex justify-center'>
-              <Image src={Cactus} weight={140} height={192} alt='Cactus illustration for the plant-themed portfolio, made by MD' />
+            <div className='group relative m-8 flex justify-center'>
+              <Image src={Cactus} height={200} alt='Cactus illustration for the plant-themed portfolio, made by MD' className="w-auto" priority={true}/>
               <span className="absolute bottom-0 scale-0 rounded bg-purple-dark p-1 text-[10px] font-roboto text-white group-hover:scale-100">✨ Je réalise aussi des illustrations ✨</span>
             </div>
           </div>
