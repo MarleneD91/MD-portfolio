d42cc67139969a7bedfa01a282f1b2b47c837737
Marlène D <marlene.a.diaz@gmail.com>
2023-08-01 15:04:16 +0200

    Adding Sakura illustrations

--- app/works/page.js
+++ app/works/page.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Image from 'next/image'
 
 import Header from '@/components/common/Header/Header'
 import Footer from '@/components/common/Footer/Footer'
+
+//Illustrations
+import LeftSakura from '@/public/images/left-sakura.png'
+import RightSakura from '@/public/images/right-sakura.png'
 
 //<main> components
 import Carousel from '@/components/works/Carousel'
@@ -20,8 +25,10 @@ const Works = () => {
       <main>
         <p className='font-gentium italic font-normal text-base mt-5 text-center'>Vous trouverez ici un aperçu des différents projets sur lesquels j’ai travaillé.<br/>
         N’hésitez pas à cliquer sur le lien des repositories Github associés! </p>
-        <div className='flex flex-row'>
+        <div className='flex flex-row justify-between my-6'>
+          <Image src={LeftSakura} height={400} width={350} alt="Illustration made by MD of a right-facing sakura branch" className='-ml-[70px]'/>
           <Carousel images={dataTest}/>
+          <Image src={RightSakura} height={400} width={350} alt="Illustration made by MD of a left-facing sakura branch" className='-mr-[70px]'/>
         </div>
         <WhatIUse elements={technoData} categoryName="Technologies "/>
         <WhatIUse elements={toolData} categoryName="Outils "/>
