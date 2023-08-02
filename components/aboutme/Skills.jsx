594b44cdb8319a632404bfacaae25cca7393a7aa
Marlène D <marlene.a.diaz@gmail.com>
2023-08-02 13:37:11 +0200

    Adding a Slide effect on hard and soft skills

--- components/aboutme/Skills.jsx
+++ components/aboutme/Skills.jsx
@@ -1,11 +1,16 @@
+"use client"
+
 import React from 'react'
 
 import Image from 'next/image'
+
+import {Slide} from 'react-awesome-reveal'
 
 import { FaBook, FaPalette, FaPagelines } from 'react-icons/fa'
 
 import RightGingkoLeaf from '@/public/images/right-gingko-leaf.png'
 import LeftGingkoLeaf from '@/public/images/left-gingko-leaf.png'
+
 
 const Skills = () => {
   return (
@@ -17,26 +22,30 @@ const Skills = () => {
             <div className='grid grid-cols-2 justify-between mx-4 h-[350px]'>
               <div className='pt-15 justify-self-start'>
                 <h3 className='font-sansita font-semibold text-xl tracking-widest my-4'>HARD SKILLS</h3>
-                <ul className='leading-8 font-gentium text-lg'>
-                  <li>Base de données</li>
-                  <li>Concepts de programmation</li>
-                  <li>Création graphique et maquettage</li>
-                  <li>Gestion de projet</li>
-                  <li>Langages de programmation</li>
-                  <li>SEO</li>
-                  <li>Versionning</li>
-                </ul>
+                <Slide>
+                  <ul className='leading-8 font-gentium text-lg'>
+                    <li>Base de données</li>
+                    <li>Concepts de programmation</li>
+                    <li>Création graphique et maquettage</li>
+                    <li>Gestion de projet</li>
+                    <li>Langages de programmation</li>
+                    <li>SEO</li>
+                    <li>Versionning</li>
+                  </ul>
+                </Slide>
               </div>
               <div className='place-self-end pt-16'>
                 <h3 className='font-sansita font-semibold text-xl tracking-widest my-4'>SOFT SKILLS</h3>
-                <ul className='leading-8 font-gentium text-lg'>
-                  <li>Autonomie</li>
-                  <li>Créativité</li>
-                  <li>Curiosité</li>
-                  <li>Organisation</li>
-                  <li>Patience</li>
-                  <li>Rigueur</li>
-                </ul>
+                <Slide direction='right'>
+                  <ul className='leading-8 font-gentium text-lg'>
+                    <li>Autonomie</li>
+                    <li>Créativité</li>
+                    <li>Curiosité</li>
+                    <li>Organisation</li>
+                    <li>Patience</li>
+                    <li>Rigueur</li>
+                  </ul>
+                </Slide>
               </div>
             </div>
             <div className='flex flex-col items-center mt-10'>
