985c4cf759a830414ea8cd547f5b7ab86152104b
Marlène D <marlene.a.diaz@gmail.com>
2023-08-02 19:46:11 +0200

    Trying to display or hide picture as a solution for changing images with themes, but maybe not the best option

--- components/common/Header/Header.jsx
+++ components/common/Header/Header.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Link from 'next/link'
 import Navbar from './Navbar'
 import Image from 'next/image'
-import DarkLogo from '../../../public/images/dark-logo.png'
+import DarkLogo from '@/public/images/dark-logo.png'
+import WhiteLogo from '@/public/images/white-logo.png'
 
 const Header = () => {
   return (
     <div className='bg-green-light  dark:bg-purple-dark flex flex-row items-center justify-between p-3 w-auto'>
-      <a href="/" className="relative"><Image src={DarkLogo} height={102} alt='Logo of MD, black version, letters and Cymothoe (butterfly) in an arrow circle' className='h-auto'/></a>
+      <a href="/" className="relative">
+        <Image src={DarkLogo} height={102} alt='Logo of MD, black version, MD letters and Cymothoe (butterfly) in an arrow circle' className='h-auto block dark:hidden'/>
+        <Image src={WhiteLogo} height={102} alt='Logo of MD, white version, MD letters and Cymothoe (butterfly) in an arrow circle' className='h-auto hidden dark:block'/>
+      </a>
       <Navbar/>
     </div>
   )
