df12f8784400dbd3530396ab1abd04ded97d7af2
Marlène D <marlene.a.diaz@gmail.com>
2023-08-02 19:52:18 +0200

    hide and display cactus vs Magnolia, it works. Is it though the right solution ?

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import HomeAnimatedText from '@/components/home/HomeAnimatedText'
 
 import Image from 'next/image'
 import Cactus from '@/public/images/Cactus.png'
+import Magnolia from '@/public/images/Magnolia.png'
 
 export default function Home() {
   return (
@@ -14,7 +15,8 @@ export default function Home() {
           <div className='flex flex-col items-center justify-between'>
             <HomeAnimatedText/>
             <div className='group relative m-5 flex justify-center'>
-              <Image src={Cactus} height={200} alt='Cactus illustration for the plant-themed portfolio, made by MD' className="w-auto" priority={true}/>
+              <Image src={Cactus} height={200} alt='Cactus in a pot illustration for the plant-themed portfolio, made by MD' className="w-auto dark:hidden" priority={true}/>
+              <Image src={Magnolia} height={300} alt='Magnolia flower illustration for the plant-themed portfolio, made by MD' className="w-auto hidden dark:block" priority={true}/>
               <span className="absolute bottom-0 scale-0 rounded bg-purple-dark p-1 text-[10px] font-roboto text-white group-hover:scale-100">✨ Je réalise aussi des illustrations ✨</span>
             </div>
           </div>
