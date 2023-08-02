b0df2d852d53c06a496048714c860d090f28a039
Marlène D <marlene.a.diaz@gmail.com>
2023-08-02 13:35:17 +0200

    Work on Projects description : crating the div and adding the elements

--- app/works/page.js
+++ app/works/page.js
@@ -17,7 +17,6 @@ import dataTest from '@/public/dataTest'
 import { technoData } from '@/public/data/technoListData.js'
 import { toolData } from '@/public/data/toolListData.js'
 
-
 const Works = () => {
   return (
     <div className='flex flex-col h-screen justify-between'>
@@ -26,10 +25,11 @@ const Works = () => {
         <p className='font-gentium italic font-normal text-base mt-5 text-center'>Vous trouverez ici un aperçu des différents projets sur lesquels j’ai travaillé.<br/>
         N’hésitez pas à cliquer sur le lien des repositories Github associés! </p>
         <div className='flex flex-row justify-between my-6'>
-          <Image src={LeftSakura} height={400} width={350} alt="Illustration made by MD of a right-facing sakura branch" className='-ml-[70px]'/>
+          <Image src={LeftSakura} width={350} alt="Illustration made by MD of a right-facing sakura branch" className='-ml-[70px] h-[400px]'/>
           <Carousel images={dataTest}/>
-          <Image src={RightSakura} height={400} width={350} alt="Illustration made by MD of a left-facing sakura branch" className='-mr-[70px]'/>
+          <Image src={RightSakura} width={350} alt="Illustration made by MD of a left-facing sakura branch" className='-mr-[70px] h-[400px]'/>
         </div>
+        {/*<Description elements={dataTest}/>*/}
         <WhatIUse elements={technoData} key={technoData.id} categoryName="Technologies "/>
         <WhatIUse elements={toolData} key={toolData.id} categoryName="Outils "/>
       </main>
--- components/works/Carousel.jsx
+++ components/works/Carousel.jsx
@@ -2,8 +2,11 @@
 
 import Image from "next/image";
 import { useState } from "react";
-import Swipe from "react-easy-swipe";
+
+
 import { FaChevronLeft, FaChevronRight, FaCircle } from 'react-icons/fa'
+
+import GithubLogo from '@/public/icons/black-github.png'
 
 const Carousel = ({ images }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -19,54 +22,58 @@ const Carousel = ({ images }) => {
   };
 
   return (
-    <div className="flex flex-col justify-center w-5/12">
-      <div className="grid grid-cols-2 justify-between items-center z-20">
-        <FaChevronLeft onClick={handlePrevSlide} className=" text-5xl cursor-pointer text-gray-400 z-20" />
-        <FaChevronRight onClick={handleNextSlide} className=" text-5xl cursor-pointer text-gray-400 z-20" />
-      </div>
-      
-      <div className="w-6/12 h-[50vh] flex overflow-hidden m-auto place-content-center">
-        <Swipe
-          onSwipeLeft={handleNextSlide}
-          onSwipeRight={handlePrevSlide}
-          className="z-10 h-100 w-100 place-items-center"
-        >
+    <div className="flex flex-col justify-center w-8/12 mb-8">
+      <div className="flex flex-row">
+        <FaChevronLeft onClick={handlePrevSlide} className=" text-5xl cursor-pointer text-gray-400 place-self-center z-20 ml-2 hover:text-gray-500" />
+        <div className="h-[300px] flex overflow-hidden m-auto place-content-center">
           {images.map((image, index) => {
-            if (index === currentSlide) {
-              return (
-                <Image
-                  key={image.id}
-                  src={image.picture}
-                  width={500}
-                  height={300}
-                  className="animate-fadeIn object-contain"
-                />
-              );
-            }
-          })}
-        </Swipe>
+              if (index === currentSlide) {
+                return (
+                  <Image key={image.id} src={image.picture} width={500} height={300} className="animate-fadeIn opacity-75 hover:opacity-100 rounded-sm w-full h-auto" alt={image.title} />
+                )
+              }
+            })}
+        </div>
+        <FaChevronRight onClick={handleNextSlide} className=" text-5xl cursor-pointer text-gray-400 place-self-center z-20 mr-2 hover:text-gray-500" />
       </div>
-
-
-      <div className="relative flex justify-center p-2 z-30">
+      <div className="relative flex justify-center pb-3 z-30">
         {images.map((_, index) => {
           return (
-            <FaCircle
-              className={
-                index === currentSlide
-                  ? "text-sm text-carousel-dot mx-1 my-2 cursor-pointer"
-                  : "text-sm text-carousel-light-dot mx-1 my-2 cursor-pointer"
-              }
-              key={index}
-              onClick={() => {
-                setCurrentSlide(index);
-              }}
-            />
-          );
+            <FaCircle className={index === currentSlide ? "text-sm text-carousel-dot mx-1 my-2 cursor-pointer" : "text-sm text-carousel-light-dot mx-1 my-2 cursor-pointer hover:text-gray-400"} key={index} onClick={() => {setCurrentSlide(index)}} />
+          )
         })}
       </div>
+      {images.map((image, index) => {
+        if(index === currentSlide){
+          return (
+            <div className=" bg-light-grey-form rounded-2xl p-4 m-4 border-2 border-zinc-400" key={image.title}>
+              <h3 className="font-sansita font-semibold text-xl tracking-wide text-center">{image.title}</h3>
+              <div className="my-4">
+              <h4 className="font-sansita text-base underline underline-offset-2">Description du projet</h4>
+                <p className="font-gentium">{image.description}</p>
+              </div>
+              <div className="my-6">
+                <h4 className="font-sansita text-base underline underline-offset-2">Diffultés rencontrées</h4>
+                <p className="font-gentium">{image.issues}</p>
+              </div>
+              <div className="flex justify-between items-end">
+                <ul>
+                  {image.technos.map((techno) => {
+                    if(index===currentSlide){
+                      return(
+                        <li className="font-roboto font-extrabold mx-2 inline text-sm" key={techno}>#{techno}</li>
+                      )
+                    }
+                  })}
+                </ul>
+                <a href={image.githubLink}><Image src={GithubLogo} height={40} width={40} alt="Logo of Github, black version, associated to a link to project's repository"/></a>
+              </div>  
+            </div>
+          )
+        }
+      })}
     </div>
-  );
+  )
 }
 
 export default Carousel
