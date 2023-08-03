4fab43385420667fdc5c83bd6d0d38588d50e751
Marlène D <marlene.a.diaz@gmail.com>
2023-08-03 08:41:53 +0200

    See previous commit

--- app/not-found.js
+++ app/not-found.js
@@ -5,14 +5,16 @@ import Footer from '@/components/common/Footer/Footer'
 
 import Image from 'next/image'
 
-import IvyBlack404 from '@/public/images/404-black-lierre.png'
+import Black404 from '@/public/images/404-black-ivy.png'
+import White404 from '@/public/images/404-white-ivy.png'
 
 export default function Custom404() {
     return (
         <div className='flex flex-col h-screen justify-between'>
             <Header/>
             <main className='flex flex-col items-center'>
-                <Image src={IvyBlack404} alt='404 illustration with climbing ivy' height={600} className='w-600 -p-10 -my-24' />
+                <Image src={Black404} alt='404 illustration with climbing ivy' height={600} className='w-600 -p-10 -my-24 dark:hidden' />
+                <Image src={White404} alt='404 illustration with climbing ivy' height={600} className='w-600 -p-10 -my-24 hidden dark:block' />
                 <p className='font-gentium font-bold tracking-wide text-xl mb-6'>La page que vous recherchez n’existe pas!</p>
                 <a href='/' className='font-roboto text-sm italic underline underline-offset-4 mt-6 mb-10'><p>Retourner à l'accueil</p></a>
             </main>
