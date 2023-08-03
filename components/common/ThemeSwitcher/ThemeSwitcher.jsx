b4606793ad967c0250d372f37e067ce7e4fbda3b
Marlène D <marlene.a.diaz@gmail.com>
2023-08-03 09:21:30 +0200

    change button light bg to dark (visibility)

--- components/common/ThemeSwitcher/ThemeSwitcher.jsx
+++ components/common/ThemeSwitcher/ThemeSwitcher.jsx
@@ -13,7 +13,7 @@ const ThemeSwitcher = () => {
       <div className='flex flex-row justify-between dark:flex-row-reverse'>
         <FaSun className='dark:hidden place-self-center ml-2 text-3xl text-sun'/>
         <FaMoon className='hidden dark:block place-self-center mr-2 text-3xl text-moon '/>
-        <button onClick={()=> setTheme( theme === "dark"? "light": "dark" )} className='rounded-full h-10 w-10 bg-light-footer-txt dark:bg-switcher-button-white z-10'></button>
+        <button onClick={()=> setTheme( theme === "dark"? "light": "dark" )} className='rounded-full h-10 w-10 bg-black dark:bg-switcher-button-white z-10'></button>
       </div>
     </div>
   )
