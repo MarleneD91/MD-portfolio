1cf5250cf64a7109575f1f933c5be210de7fd492
Marlène D <marlene.a.diaz@gmail.com>
2023-08-03 09:22:22 +0200

    Changing universities logos style for better visibility in dark mode

--- components/aboutme/Curriculum.jsx
+++ components/aboutme/Curriculum.jsx
@@ -24,21 +24,21 @@ const Curriculum = () => {
             <div className='flex flex-row m-4'>
                 <Image src={OPC} height={70} alt='OpenClassrooms logo' className='w-[70px]'/>
                 <FaCircle className='text-[6px] place-self-center ml-4'/>
-                <hr className='w-1/12 border-black border-[1px] place-self-center mr-4'/>
+                <hr className='w-1/12 border-black border-[1px] place-self-center mr-4 dark:border-white'/>
                 <Fade><p className='place-self-center font-gentium font-normal tracking-wide text-lg'>Développeur intégrateur web - Certification RCPN (bac+2)</p></Fade>
             </div>
             <h3 className='font-gentium tracking-wider font-bold text-2xl mt-10'>2015</h3>
             <div className='flex flex-row m-4'>
                 <Fade><p className='place-self-center font-gentium font-normal tracking-wide text-lg'>Master 2 Toxicologie Environnement Santé</p></Fade>
-                <hr className='w-1/12 border-black border-[1px] place-self-center ml-4'/>
+                <hr className='w-1/12 border-black border-[1px] place-self-center ml-4 dark:border-white'/>
                 <FaCircle className='text-[6px] place-self-center mr-2'/>
-                <Image src={ParisDiderot} height={160} alt='Paris Diderot (P7) University Logo' className='rotate-90 ml-28 -mt-20 -mb-20 w-[70px]'/>
+                <Image src={ParisDiderot} height={160} alt='Paris Diderot (P7) University Logo' className='rotate-90 ml-28 -mt-20 -mb-20 w-[70px] dark:w-[82px] dark:bg-white  dark:border-4 dark:border-white'/>
             </div>
             <h3 className='font-gentium tracking-wider font-bold text-2xl mt-10'>2013</h3>
             <div className='flex flex-row justify-center items-center align-middle m-4'>
-                <Image src={UJF} height={60} alt='Joseph Fourier (Grenoble) University Logo' className='w-[305px]'/>
+                <Image src={UJF} height={60} alt='Joseph Fourier (Grenoble) University Logo' className='w-[305px] dark:bg-white'/>
                 <FaCircle className='text-[6px] place-self-center ml-2'/>
-                <hr className='w-1/12 border-black border-[1px] place-self-center mr-4'/>
+                <hr className='w-1/12 border-black border-[1px] place-self-center mr-4 dark:border-white'/>
                 <Fade><p className='place-self-center font-gentium font-normal tracking-wide text-lg'>Master 2 Biologie Écologie Environnement</p></Fade>
             </div>
         </div>
