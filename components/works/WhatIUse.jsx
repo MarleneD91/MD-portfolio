c0ae474a864212a5ff0e7e8570c1cd220a9a7a81
Marlène D <marlene.a.diaz@gmail.com>
2023-08-08 12:16:19 +0200

    changing from next <Image/> to icon

--- components/works/WhatIUse.jsx
+++ components/works/WhatIUse.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 
-import Image from 'next/image'
-
-
 const WhatIUse = ({elements, categoryName}) => {
     return (
         <div className='flex flex-row mb-6 ml-6'>
             <h2 className='font-sansita font-bold text-3xl p-4 mb-4 ml-3 text-center'>{categoryName} :</h2>
-            {elements.map(element => 
-                <div className='group relative flex justify-center'>
-                    <Image src={element.icon} alt={element.alt} key={element.id} width={70} className='m-2 place-self-center h-auto' />
-                    <span className="absolute bottom-0 scale-0 rounded bg-purple-dark p-1 text-[10px] font-roboto text-white group-hover:scale-100">{element.id}</span>
-                </div>
-            )} 
+            <div className='flex flex-row items-center'>
+                {elements.map((element) => 
+                    <div className='group relative flex justify-center' key={element.id}>
+                        <div className='text-5xl mx-2'>{element.icon}</div>
+                        <span className="absolute bottom-0 scale-0 rounded bg-purple-dark p-1 text-[10px] font-roboto text-white group-hover:scale-100">{element.id}</span>
+                    </div>
+                )} 
+            </div>
         </div>
     )
 }
