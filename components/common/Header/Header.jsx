9bfebdcef2574a070029501b2ffcd6d1eb8880cf
Marlène D <marlene.a.diaz@gmail.com>
2023-08-08 12:14:41 +0200

    ThemeIm component used for switching logo (b) modes

--- components/common/Header/Header.jsx
+++ components/common/Header/Header.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
-import Link from 'next/link'
+
 import Navbar from './Navbar'
-import Image from 'next/image'
-import DarkLogo from '@/public/images/dark-logo.png'
+
+import ThemeImg from '../ThemeImg'
+import BlackLogo from '@/public/images/dark-logo.png'
 import WhiteLogo from '@/public/images/white-logo.png'
 
 const Header = () => {
   return (
     <div className='bg-green-light  dark:bg-purple-dark flex flex-row items-center justify-between p-3 w-auto'>
       <a href="/" className="relative">
-        <Image src={DarkLogo} height={102} alt='Logo of MD, black version, MD letters and Cymothoe (butterfly) in an arrow circle' className='h-auto block dark:hidden'/>
-        <Image src={WhiteLogo} height={102} alt='Logo of MD, white version, MD letters and Cymothoe (butterfly) in an arrow circle' className='h-auto hidden dark:block'/>
+        <ThemeImg LightImage={BlackLogo}
+                  DarkImage={WhiteLogo}
+                  height={102}
+                  width={102}
+                  altLight={'Logo of MD, black version, MD letters and Cymothoe (butterfly) in an arrowed circle'} 
+                  altDark={'Logo of MD, white version, MD letters and Cymothoe (butterfly) in an arrowed circle'}
+                  className='h-auto'
+        />
+        {/*<Image src={DarkLogo} height={102} alt='Logo of MD, black version, MD letters and Cymothoe (butterfly) in an arrow circle' className='h-auto block dark:hidden'/>
+        <Image src={WhiteLogo} height={102} alt='Logo of MD, white version, MD letters and Cymothoe (butterfly) in an arrow circle' className='h-auto hidden dark:block'/>*/}
       </a>
       <Navbar/>
     </div>
