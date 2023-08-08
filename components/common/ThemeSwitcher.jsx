ddd05f16085a75f8f8b154e31949244e2120ff9c
Marlène D <marlene.a.diaz@gmail.com>
2023-08-08 12:13:40 +0200

    ThemeSwhitcher : button to change the mode (b) light and dark

---
+++ components/common/ThemeSwitcher.jsx
@@ -0,0 +1,22 @@
+"use client"
+
+import React from 'react'
+
+import { useTheme } from 'next-themes'
+
+import { FaMoon, FaSun } from 'react-icons/fa'
+
+const ThemeSwitcher = () => {
+  const {theme, setTheme} = useTheme()
+  return (
+    <div  className='fixed bottom-10 right-4 w-[85px] h-10 rounded-[35px] bg-switcher-sun-bg dark:bg-switcher-moon-bg z-0'>
+      <div className='flex flex-row justify-between dark:flex-row-reverse'>
+        <FaSun className='dark:hidden place-self-center ml-2 text-3xl text-sun'/>
+        <FaMoon className='hidden dark:block place-self-center mr-2 text-3xl text-moon '/>
+        <button onClick={()=> setTheme( theme === "dark"? "light": "dark" )} className='rounded-full h-10 w-10 bg-black dark:bg-switcher-button-white z-10'></button>
+      </div>
+    </div>
+  )
+}
+
+export default ThemeSwitcher
