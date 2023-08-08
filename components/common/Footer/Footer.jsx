a9520e4861c458ff410bfb4007a8bd9243e977bc
Marlène D <marlene.a.diaz@gmail.com>
2023-08-08 12:14:15 +0200

    Replace png with react-icons allows automatically switching text color

--- components/common/Footer/Footer.jsx
+++ components/common/Footer/Footer.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
 
-//NextJS Image component
-import Image from 'next/image'
+//Font Awesome Icons
+import { FaCircle, FaGithub, FaLinkedin } from "react-icons/fa";
 
-//Font Awesome Icon
-import { FaCircle } from "react-icons/fa";
-//Footer Logos
-import BlackGithub from '@/public/icons/black-github.png'
-import BlackLinkedin from '@/public/icons/black-linkedin.png'
+
 
 const Footer = () => {
   return (
@@ -15,10 +11,10 @@ const Footer = () => {
       <div className='font-gentium p-1 justify-self-start text-light-footer-txt dark:text-dark-footer-txt'>
         <p>© Marlène DIAZ | 2023</p>
       </div>
-      <div className='align-middle grid grid-cols-3 gap-0.1 justify-self-center items-center'>
-        <a href="github link"><Image src={BlackGithub} height={35} alt='Logo of Github, black version, associated to a link to the author`s github profile' /></a>
-        <FaCircle className='text-xs justify-self-center black dark:white'/>
-        <a href="linkedin link"><Image src={BlackLinkedin} height={35} alt='Logo of Linkedin, black version, associated to a link to the author`s linkedin profile'/></a>
+      <div className='align-middle grid grid-cols-3 gap-0.1 justify-self-center items-center text-4xl'>
+        <a href="github link"><FaGithub /></a>
+        <FaCircle className='text-xs justify-self-center'/>
+        <a href="linkedin link"><FaLinkedin /></a>
       </div>
     </div>
   )
