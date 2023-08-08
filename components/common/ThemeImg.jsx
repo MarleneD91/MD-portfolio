5d284d9584b87f71494dc21b1abe8a10df5e5e8d
Marlène D <marlene.a.diaz@gmail.com>
2023-08-08 12:13:10 +0200

    Theme Img - Components that allow a change of image src when switching (b) Light and dark mode

---
+++ components/common/ThemeImg.jsx
@@ -0,0 +1,24 @@
+"use client"
+
+import React from 'react'
+
+import Image from 'next/image'
+
+import { useTheme } from 'next-themes'
+
+export const ThemeImg = ({LightImage, DarkImage, height, width, altLight, altDark}) => {
+    const {theme, setTheme} = useTheme()
+        if(theme === "dark"){
+            return (
+                <Image src={DarkImage} height={height} width={width} alt={altLight} priority={true} className='object-cover'/>
+            )
+            } else {
+            return(
+                <Image src={LightImage} height={height} width={width} alt={altDark} priority={true} className='object-cover'/> 
+            )
+        }
+        
+
+    }
+
+export default ThemeImg
