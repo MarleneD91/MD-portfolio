d8307a184c0512f87cf66518b873eef887452f85
Marlène D <marlene.a.diaz@gmail.com>
2023-08-14 21:57:15 +0200

    !!!! Do not use NextRequest (error default.json not a function) but req + NextResponse

---
+++ app/api/contact/route.js
@@ -0,0 +1,22 @@
+import connect from "@/lib/dbConfig"; // Connection to db
+
+import contactModel from "@/models/contactModel"; // Data model for form info
+
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+    
+     try {
+        const { name, firstname, email, message } = await req.json()
+
+        await connect()
+
+        await contactModel.create({ name, firstname, email, message })
+
+        return NextResponse.json({message: "Message sent!"}, {status: 200})
+
+    } catch(err) {
+        return NextResponse.json({message: "Something went wrong while sending the message : " + err}, {status: 400})
+    }
+
+}
