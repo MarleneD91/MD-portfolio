272e02a1e85ac543b5898e702e99a7d3ac7f07ca
Marlène D <marlene.a.diaz@gmail.com>
2023-08-16 11:03:36 +0200

    manage  show modal  and close modal

--- components/admin/works-management/AddWorkForm.jsx
+++ components/admin/works-management/AddWorkForm.jsx
@@ -10,7 +10,14 @@ import { useRouter } from 'next/navigation'
 
 import Image from 'next/image'
 
+import { TbX } from "react-icons/tb"
+
 const AddWorkForm = () => {
+
+  const [addWorkModal, setAddWorkModal] = useState(true)
+  const closeModal = () => {
+    setAddWorkModal(!addWorkModal)
+  }
 
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("")
@@ -32,24 +39,27 @@ const AddWorkForm = () => {
         "Content-type": "application/json",
       },
       body: JSON.stringify({
-        title: title,
-        description: description,
-        issues: issues,
-        technos: technos,
-        githubLink: githubLink,
-        imageUrl: imageUrl
+        title,
+        description,
+        issues,
+        technos,
+        githubLink,
+        imageUrl
       }),
     });
 
-    const { msg, success } = await res.json();
-    setError(msg);
-    setSuccess(success);
-
-    if (success) {
+    if(res.status === 200){
+      setSuccess(true);
       alert("Work added!")
+      
+      setName("");
+      setFirstname("");
+      setEmail("");
+      setMessage("");
+      
       router.push('/admin')
-    }else{
-      alert("Something went wrong!")
+    } else {
+      alert("Something went wrong while creating the project.")
     }
   };
 
@@ -63,80 +73,92 @@ const AddWorkForm = () => {
   }
   return (
     <>
-    <form
-      onSubmit={handleSubmit}
-      className="p-6 my-6 m-auto w-5/12 border border-1 border-gray-300 dark:border-gray-500 rounded-[35px] bg-light-gray-form dark:bg-dark-gray-form flex flex-col gap-5"
-    >
-      <div className="flex flex-col mx-4">
-        <label htmlFor="title" className="w-11/12 place-self-start my-2">Nom du projet</label>
-        <input
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}
-          type="text"
-          id="title"
-          placeholder="Nom du projet"
-          className="rounded-md p-1 dark:bg-dark-blue-form"
-          autoComplete="off"
-        />
+    {addWorkModal ?
+      <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50">
+        <div className='relative max-h-fit mt-36 pt-12 p-6 mb-6 mx-auto  w-6/12  flex flex-col z-60'>
+          <div className="flex flex-col p-8 mt-48 border border-1 border-gray-300 dark:border-gray-500 rounded-[35px] bg-light-gray-form dark:bg-dark-gray-form">
+          <div className='flex flex-row m-2 mb-6 items-center justify-between'>
+            <h2 className='text-2xl font-sansita w-full text-center'>Ajouter un projet</h2>
+            <div onClick={closeModal} className='text-end self-start text-3xl'><TbX/></div>
+          </div>
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col gap-5"
+          >
+            <div className="flex flex-col mx-4">
+              <label htmlFor="title" className="w-11/12 place-self-start my-2">Nom du projet</label>
+              <input
+                onChange={(e) => setTitle(e.target.value)}
+                value={title}
+                type="text"
+                id="title"
+                placeholder="Nom du projet"
+                className="rounded-md p-1 dark:bg-dark-blue-form"
+                autoComplete="off"
+              />
+            </div>
+            <div className="flex flex-col mx-4">
+              <label htmlFor="description" className="w-11/12 place-self-start my-2">Description</label>
+              <textarea
+                onChange={(e) => setDescription(e.target.value)}
+                value={description}
+                id="description"
+                placeholder="Décrivez le projet..."
+                className="rounded-md p-1 dark:bg-dark-blue-form h-28"
+                autoComplete="off"
+              />
+            </div>
+            <div className="flex flex-col mx-4">
+              <label htmlFor="issues" className="w-11/12 place-self-start my-2">Difficultées rencontrées</label>
+              <textarea
+                onChange={(e) => setIssues(e.target.value)}
+                value={issues}
+                id="issues"
+                placeholder="Quelles ont été les difficultés rencontrées ?"
+                className="rounded-md p-1 dark:bg-dark-blue-form h-24"
+                autoComplete="off"
+              />
+            </div>
+            <div className="flex flex-col mx-4">
+              <label htmlFor='technos' className='w-11/12 place-self-start my-2'>Technologies utilisées</label>
+              <input
+              onChange={handleTechnos}
+              value={technos}
+              id='technos'
+              placeholder='Technologies utilisées'
+              autoComplete='off'
+              />
+            </div>
+            <div className="flex flex-col mx-4">
+              <label htmlFor="githubLink" className="w-11/12 place-self-start my-2">Lien Github</label>
+              <input
+                onChange={(e) => setGithubgLink(e.target.value)}
+                value={githubLink}
+                type="url"
+                id="githubLink"
+                placeholder="Lien du repository Github"
+                className="rounded-md p-1 dark:bg-dark-blue-form"
+                autoComplete="off"
+              />
+            </div>
+            <div className="flex flex-col mx-4">
+              <label htmlFor="imageUrl" className="w-11/12 place-self-start my-2">Image</label>
+              <input
+                onChange={handleImg}
+                type="file"
+                id="imageUrl"/>
+              {!imageUrl ? <></> : <Image src={imageUrl} width={250} height={200} alt="Image of the project"/>}
+            </div>
+            
+            <button className="bg-black text-white w-3/12 place-self-center px-2 pb-1 rounded-xl font-sansita font-regular" type="submit">
+              {!success ? "Ajouter" : "Projet ajouté!"}
+            </button>
+          </form>
+          </div>
+        </div>
       </div>
-      <div className="flex flex-col mx-4">
-        <label htmlFor="description" className="w-11/12 place-self-start my-2">Description</label>
-        <textarea
-          onChange={(e) => setDescription(e.target.value)}
-          value={description}
-          id="description"
-          placeholder="Décrivez le projet..."
-          className="rounded-md p-1 dark:bg-dark-blue-form h-28"
-          autoComplete="off"
-        />
-      </div>
-      <div className="flex flex-col mx-4">
-        <label htmlFor="issues" className="w-11/12 place-self-start my-2">Difficultées rencontrées</label>
-        <textarea
-          onChange={(e) => setIssues(e.target.value)}
-          value={issues}
-          id="issues"
-          placeholder="Quelles ont été les difficultés rencontrées ?"
-          className="rounded-md p-1 dark:bg-dark-blue-form h-24"
-          autoComplete="off"
-        />
-      </div>
-      <div className="flex flex-col mx-4">
-        <label htmlFor='technos' className='w-11/12 place-self-start my-2'>Technologies utilisées</label>
-        <input
-        onChange={handleTechnos}
-        value={technos}
-        id='technos'
-        placeholder='Technologies utilisées'
-        autoComplete='off'
-        />
-      </div>
-      <div className="flex flex-col mx-4">
-        <label htmlFor="githubLink" className="w-11/12 place-self-start my-2">Lien Github</label>
-        <input
-          onChange={(e) => setGithubgLink(e.target.value)}
-          value={githubLink}
-          type="url"
-          id="githubLink"
-          placeholder="Lien du repository Github"
-          className="rounded-md p-1 dark:bg-dark-blue-form"
-          autoComplete="off"
-        />
-      </div>
-      <div className="flex flex-col mx-4">
-        <label htmlFor="imageUrl" className="w-11/12 place-self-start my-2">Image</label>
-        <input
-          onChange={handleImg}
-          type="file"
-          id="imageUrl"/>
-        {!imageUrl ? <></> : <Image src={imageUrl} width={250} height={200} alt="Image of the project"/>}
-      </div>
-      
-      <button className="bg-black text-white w-3/12 place-self-center px-2 pb-1 rounded-xl font-sansita font-regular" type="submit">
-        Ajouter
-      </button>
-    </form>
-  </>
+      : <></> }
+    </>
   )
 }
 
