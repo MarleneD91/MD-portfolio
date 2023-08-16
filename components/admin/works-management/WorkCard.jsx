1e878f5958d5e0e2a1c17bc637e0c89eaede544c
Marlène D <marlene.a.diaz@gmail.com>
2023-08-16 11:05:06 +0200

    handleWorkDeletion (need to work on it) + editmodal show/hide ( ? : )

--- components/admin/works-management/WorkCard.jsx
+++ components/admin/works-management/WorkCard.jsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 
 import { FaTrashAlt, FaPencilAlt } from "react-icons/fa"
 
-
+import EditWorkForm from "@/components/admin/works-management/EditWorkForm"
 
 const WorkCard = ({works}) => {
 
@@ -15,32 +15,42 @@ const WorkCard = ({works}) => {
     setEditWorkModal(!editWorkModal)
   }
 
-  const handleWorkDeletion = () => {
-    return("Work deleted!")
+  const handleWorkDeletion = async () => {
+    const confirmDeletion = confirm("Êtes vous sûr.e de voulr supprimer ce projet ?")
+
+    if(confirmDeletion){
+      const res = await fetch("api/works/:id",{
+        method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+      },
+      })
+    }
   }
 
   return (
     <>
-      {works.map(work =>
-        <div className='w-5/6 h-5/6 m-4  bg-light-gray-form dark:bg-dark-gray-form p-4 rounded-md' key={work.title}>
-          <Image src={work.picture} height={300} width={300} alt={work.title} className='h-1/2 w-full object-cover mb-3'/>
-          <hr className='w-5/6'/>
-          <div className='text-xs my-3 line-clamp-3'>
-            {work.description}
-          </div>
-          <hr className='w-5/6'/>
-          <div>
-            <div className='flex flex-row text-l mt-2 items-center' onClick={manageEditModal}>
-              <FaPencilAlt className='mr-2'/> <span>Éditer</span>
+      {editWorkModal ? <EditWorkForm/> : <></>}
+      <>
+        {works.map(work =>
+          <div className='w-5/6 h-5/6 m-4  bg-light-gray-form dark:bg-dark-gray-form p-4 rounded-md' key={work.title}>
+            <Image src={work.picture} height={300} width={300} alt={work.title} className='h-1/2 w-full object-cover mb-3'/>
+            <hr className='w-5/6'/>
+            <div className='text-xs my-3 line-clamp-3'>
+              {work.description}
             </div>
-            {/* Add an onclick fonction to open a modal to edit the project -the supp/edit modal can be a component!? */}
-            <div className='flex flex-row text-l mt-2 items-center' onClick={handleWorkDeletion}>
-              <FaTrashAlt className='mr-2'/>  <span>Supprimer</span>
+            <hr className='w-5/6'/>
+            <div>
+              <div className='flex flex-row text-l mt-2 items-center' onClick={manageEditModal}>
+                <FaPencilAlt className='mr-2'/> <span>Éditer</span>
+              </div>
+              <div className='flex flex-row text-l mt-2 items-center' onClick={handleWorkDeletion}>
+                <FaTrashAlt className='mr-2'/>  <span>Supprimer</span>
+              </div>
             </div>
-            {/* Add an onclick fonction to open a modal to supp the project */}
           </div>
-        </div>
-      )}
+        )}
+      </> 
     </>
   )
 }
