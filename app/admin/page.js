14d6d3649c8a5dd374f268975ba3767f50952073
Marlène D <marlene.a.diaz@gmail.com>
2023-08-16 11:00:09 +0200

    try with react-cookie - but impossible to logout with removeCookie

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,24 +1,43 @@
 "use client"
 
+import { useState, useEffect } from 'react';
+
 import SignInForm from '@/components/admin/SignInForm'
 
 import { CookiesProvider, useCookies } from "react-cookie";
-import Dashboard from '../admin/page';
-import SignUpForm from '@/components/admin/SignupForm';
+import Dashboard from '@/components/admin/Dashboard';
+
+import { useRouter } from 'next/navigation';
 
 const Admin = () => {
-    const [cookies, setCookies] = useCookies()
-  
+    const [cookies, setCookies, removeCookies ] = useCookies(["user"])
+    const router = useRouter()
+
+    function handleLogin(user) {
+      setCookies("user", user, { path: "/admin", secure: "true", sameSite: "none" });
+      router.push('/admin')
+      console.log(cookies.user)
+    }
+
+    function handleLogout(){
+      setCookies("user", "", { path: "/admin", secure: "true", sameSite: "none" })
+      router.push('/')
+      console.log(cookies.user)
+    }
+
+    const isConnected = () =>{
+      cookies.user.value.length > 10;
+      isClient(true);
+    }
+
   return (
     <CookiesProvider>
-        {cookies.user ? 
-            (<Dashboard/>)
+        { isConnected ? 
+            (<Dashboard onLogout={handleLogout}/>)
            : (<div className='mt-28' >
-                <SignInForm />      
-            </div>)}
+                <SignInForm onLogin={handleLogin}/>      
+              </div>)}
     </CookiesProvider>
-
-
   )
 }
 
