d63a18e5320b4238c80fbc30f6a81f9564da612e
Marlène D <marlene.a.diaz@gmail.com>
2023-08-17 17:05:02 +0200

    isConnected : token stored + handlelogout: clear localStorage

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,43 +1,32 @@
 "use client"
 
-import { useState, useEffect } from 'react';
-
 import SignInForm from '@/components/admin/SignInForm'
 
-import { CookiesProvider, useCookies } from "react-cookie";
+
 import Dashboard from '@/components/admin/Dashboard';
 
 import { useRouter } from 'next/navigation';
 
 const Admin = () => {
-    const [cookies, setCookies, removeCookies ] = useCookies(["user"])
+
     const router = useRouter()
 
-    function handleLogin(user) {
-      setCookies("user", user, { path: "/admin", secure: "true", sameSite: "none" });
-      router.push('/admin')
-      console.log(cookies.user)
-    }
+    const isConnected = localStorage.getItem("token")
 
     function handleLogout(){
-      setCookies("user", "", { path: "/admin", secure: "true", sameSite: "none" })
+      localStorage.clear()
       router.push('/')
-      console.log(cookies.user)
-    }
-
-    const isConnected = () =>{
-      cookies.user.value.length > 10;
-      isClient(true);
     }
 
   return (
-    <CookiesProvider>
+    <>
         { isConnected ? 
             (<Dashboard onLogout={handleLogout}/>)
            : (<div className='mt-28' >
-                <SignInForm onLogin={handleLogin}/>      
+                <SignInForm />      
               </div>)}
-    </CookiesProvider>
+               {console.log(isConnected)}
+    </>
   )
 }
 
