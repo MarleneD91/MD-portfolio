bf44ed2fd94faf0abef505d926a84ac81c8c08dc
Marlène D <marlene.a.diaz@gmail.com>
2023-08-21 20:58:32 +0200

    @mq : text style

--- app/(root)/contact/page.js
+++ app/(root)/contact/page.js
@@ -10,7 +10,7 @@ const Contact = () => {
     <div className='flex flex-col h-screen justify-between'>
     <Header/>
     <main>
-      <p className='font-gentium italic font-normal text-base self-start text-center my-5'>Vous souhaitez me contacter ? N’hésitez pas à m’envoyer un message à l’aide du formulaire ci-dessous. <br/>
+      <p className='font-gentium italic font-normal text-base self-start text-center my-5 sm-devices:mx-3'>Vous souhaitez me contacter ? N’hésitez pas à m’envoyer un message à l’aide du formulaire ci-dessous. <br/>
       Je m’engage à vous répondre dans les plus brefs délais. </p>
       <ContactForm/>
     </main>
