cee7cc501b9bd917d807514b75527c94da495edc
Marlène D <marlene.a.diaz@gmail.com>
2023-08-28 08:23:53 +0200

    Changes for mq

--- app/(root)/works/page.js
+++ app/(root)/works/page.js
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+
 import Image from 'next/image'
 
 import Header from '@/components/common/Header/Header'
@@ -13,39 +14,28 @@ import Carousel from '@/components/works/Carousel'
 import WhatIUse from '@/components/works/WhatIUse'
 
 //data [Carousel + tehnos/tools icons]
-import dataTest from '@/public/dataTest'
 import { technoData } from '@/public/data/technoListData.js'
 import { toolData } from '@/public/data/toolListData.js'
 
-const Works = () => {
 
-  const getWorksData = async () =>{
-    const res = await fetch('/api/works',{
-      method: 'GET',
-      headers: {
-        "Content-type": "application/json",
-      },
-    })
-    const resData = res.json()
-    return resData
-  }
-  const getWorks = getWorksData();
-  console.log(getworks)
+
+const Works = () => {
 
   return (
     <div className='flex flex-col h-screen justify-between'>
       <Header/>
-      <main>
-        <p className='font-gentium italic font-normal text-base mt-5 text-center'>Vous trouverez ici un aperçu des différents projets sur lesquels j’ai travaillé.<br/>
+      <main className='mobiles:flex mobiles:flex-col  mobiles:items-center mobiles:-my-5'>
+        <Image src={LeftSakura} width={160} alt="Illustration made by MD of a right-facing sakura branch" className='hidden mobiles:flex rotate-90 h-[180px] w-[144px] mobile-sm:h-[160px] mobile-sm:w-[128px]' priority={false} />
+        <p className='font-gentium italic font-normal text-base mt-5 text-center tablet:text-sm mobile:text-sm mobiles:mx-2 mobile-sm:text-[12px] mobile-sm:mt-3 mobile-sm:leading-5'>Vous trouverez ici un aperçu des différents projets sur lesquels j’ai travaillé.<br/>
         N’hésitez pas à cliquer sur le lien des repositories Github associés! </p>
-        <div className='flex flex-row justify-between my-6'>
-          <Image src={LeftSakura} width={350} alt="Illustration made by MD of a right-facing sakura branch" className='-ml-[70px] h-[400px]'/>
-          <Carousel works={getWorks}/>
-          <Image src={RightSakura} width={350} alt="Illustration made by MD of a left-facing sakura branch" className='-mr-[70px] h-[400px]'/>
+        <div className='flex flex-row justify-between my-6 mobiles:justify-center mobiles:mb-2'>
+          <Image src={LeftSakura} width={160} alt="Illustration made by MD of a right-facing sakura branch" className='h-[200px] sm-devices:w-[120px] sm-devices:h-[150px] mobiles:hidden' priority={false} />
+          <Carousel />
+          <Image src={RightSakura} width={160} alt="Illustration made by MD of a left-facing sakura branch" className='h-[200px] sm-devices:w-[120px] sm-devices:h-[150px] mobiles:hidden' priority={false} />
         </div>
-        {/*<Description elements={dataTest}/>*/}
         <WhatIUse elements={technoData} key={technoData.id} categoryName="Technologies "/>
         <WhatIUse elements={toolData} key={toolData.id} categoryName="Outils "/>
+        <Image src={RightSakura} width={160} alt="Illustration made by MD of a left-facing sakura branch" className='hidden mobiles:flex rotate-90 h-[180px] w-[144px] mobile-sm:h-[160px] mobile-sm:w-[128px]' priority={false} />
       </main>
       <Footer/>
     </div>
