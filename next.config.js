b594dc49b013f421dff9e3492bb6a32e3b0ec891
Marlène D <marlene.a.diaz@gmail.com>
2023-08-28 08:22:53 +0200

    + form-data, path, fs, mime

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains : ['s3-eu-west-1.amazonaws.com'],
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 's3-eu-west-1.amazonaws.com',
-          port: '',
-          pathname: '/course.oc-static.com/projects/front-end-kasa-project//**',
-        },
-      ],
-    },
+
   }
 
 module.exports = nextConfig
