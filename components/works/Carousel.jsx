4a1f7ec0828ea8ed53f84b1d45507d9115751eef
Marlène D <marlene.a.diaz@gmail.com>
2023-08-28 08:28:08 +0200

    useEffect, get works data

--- components/works/Carousel.jsx
+++ components/works/Carousel.jsx
@@ -1,14 +1,27 @@
 "use client"
 
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 import { FaChevronLeft, FaChevronRight, FaCircle, FaGithub } from 'react-icons/fa'
 
 
-const Carousel = ({ works }) => {
+
+
+
+const Carousel = () => {
+
+  const [works, setWorks] = useState(null)
+ 
   const [currentSlide, setCurrentSlide] = useState(0);
+
+
+  useEffect(()=>{    
+   getWorksData()
+  }, [])
+
+ 
 
   const handleNextSlide = () => {
     let newSlide = currentSlide === works.length - 1 ? 0 : currentSlide + 1;
@@ -20,47 +33,59 @@ const Carousel = ({ works }) => {
     setCurrentSlide(newSlide);
   };
 
+const getWorksData = async () => {
+  const res = await fetch('../api/works',{
+    method: 'GET',
+    headers: {
+      "Content-type": "application/json",
+    },
+  })
+  const resData = await res.json()
+  console.log(resData)
+  setWorks(resData)
+  console.log(works)
+}
   return (
-    <div className="flex flex-col justify-center w-8/12 mb-8">
+    <div className="flex flex-col justify-center w-1/2 mb-8 mobiles:w-11/12 mobiles:mb-4">
       <div className="flex flex-row">
-        <FaChevronLeft onClick={handlePrevSlide} className=" text-5xl cursor-pointer text-gray-400 place-self-center z-20 ml-2 hover:text-gray-500" />
-        <div className="h-[300px] overflow-hidden m-auto place-content-center">
-          {works.map((work, index) => {
+        <FaChevronLeft onClick={handlePrevSlide} className=" text-5xl sm-devices:text-4xl mobile:text-3xl mobile-sm:text-2xl cursor-pointer text-gray-400 place-self-center z-20 ml-2 hover:text-gray-500 mobile-sm:ml-0" />
+        <div className="h-[250px] overflow-hidden m-auto place-content-center sm-devices:h-[180px] mobile:h-[220px] mobile-sm:h-[200px]">
+          {works && works.map((work, index) => {
               if (index === currentSlide) {
                 return (
-                  <Image key={work.title} src={work.imageUrl} width={500} height={300} className="opacity-75 hover:opacity-100 rounded-sm h-full w-full object-cover" alt={work.title} />
+                  <Image key={work.title} src={work.imageUrl} width={500} height={300} className="opacity-75 hover:opacity-100 rounded-sm h-full w-full object-cover mobile:w-[300px] mobile-sm:w-[175px]" alt={work.title} />
                 )
               }
             })}
         </div>
-        <FaChevronRight onClick={handleNextSlide} className=" text-5xl cursor-pointer text-gray-400 place-self-center z-20 mr-2 hover:text-gray-500" />
+        <FaChevronRight onClick={handleNextSlide} className=" text-5xl sm-devices:text-4xl mobile:text-3xl mobile-sm:text-2xl cursor-pointer text-gray-400 place-self-center z-20 mr-2 hover:text-gray-500 mobile-sm:mr-0" />
       </div>
       <div className="relative flex justify-center pb-3 z-30">
-        {works.map((_, index) => {
+        {works && works.map((_, index) => {
           return (
-            <FaCircle className={index === currentSlide ? "text-sm text-carousel-dot dark:text-carousel-light-dot mx-1 my-2 cursor-pointer" : "text-sm text-carousel-light-dot dark:text-carousel-dot mx-1 my-2 cursor-pointer hover:text-gray-400"} key={index} onClick={() => {setCurrentSlide(index)}} />
+            <FaCircle className={index === currentSlide ? "text-sm sm-devices:text-[10px] mobile-sm:text-[8px] text-carousel-dot dark:text-carousel-light-dot mx-1 my-2 cursor-pointer" : "text-sm sm-devices:text-[10px] mobile-sm:text-[8px] text-carousel-light-dot dark:text-carousel-dot mx-1 my-2 cursor-pointer hover:text-gray-400"} key={index} onClick={() => {setCurrentSlide(index)}} />
           )
         })}
       </div>
-      {works.map((work, index) => {
+      {works && works.map((work, index) => {
         if(index === currentSlide){
           return (
             <div className=" bg-light-gray-form dark:bg-dark-gray-form rounded-2xl p-4 m-4 border-2 border-zinc-400" key={work.title}>
-              <h3 className="font-sansita font-semibold text-xl tracking-wide text-center">{work.title}</h3>
+              <h3 className="font-sansita font-semibold text-2xl tracking-wide text-center">{work.title}</h3>
               <div className="my-4">
               <h4 className="font-sansita text-base underline underline-offset-2">Description du projet</h4>
-                <p className="font-gentium">{work.description}</p>
+                <p className="font-gentium text-sm mobile-sm:text-[13px]">{work.description}</p>
               </div>
               <div className="my-6">
                 <h4 className="font-sansita text-base underline underline-offset-2">Diffultés rencontrées</h4>
-                <p className="font-gentium">{work.issues}</p>
+                <p className="font-gentium text-sm mobile-sm:text-[13px]">{work.issues}</p>
               </div>
               <div className="flex justify-between items-end">
                 <ul>
                   {work.technos.map((techno) => {
                     if(index===currentSlide){
                       return(
-                        <li className="font-roboto font-extrabold mx-2 inline text-sm" key={techno}>#{techno}</li>
+                        <li className="font-roboto font-extrabold mx-2 inline text-sm mobile-sm:text-[12px]" key={techno}>#{techno}</li>
                       )
                     }
                   })}
