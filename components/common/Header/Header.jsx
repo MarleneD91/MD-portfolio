cf9bf0a3496320cd3179bd58131bfe1fdc88ccd9
Marlène D <marlene.a.diaz@gmail.com>
2023-08-28 08:27:22 +0200

    logo size // mq

--- components/common/Header/Header.jsx
+++ components/common/Header/Header.jsx
@@ -8,15 +8,16 @@ import WhiteLogo from '@/public/images/white-logo.png'
 
 const Header = () => {
   return (
-    <div className='bg-green-light  dark:bg-purple-dark flex flex-row items-center justify-between p-3 w-auto sm-devices:flex-col sm-devices:max-w-[100vw]'>
-      <a href="/" className="relative">
+    <div className='bg-green-light  dark:bg-purple-dark flex flex-row items-center justify-between p-3 w-auto sm-devices:flex-col sm-devices:max-w-[100vw] mobiles:pt-1'>
+      <a href="/" className="mobile-sm:h-[40px] mobile-sm:w-[40px] mobile:place-self-start mobile:mt-2 mobile:h-[50px] mobile:w-[50px]">
         <ThemeImg LightImage={BlackLogo}
                   DarkImage={WhiteLogo}
-                  height={102}
-                  width={102}
+                  height={100}
+                  width={100}
+                  layout="responsive"
                   altLight={'Logo of MD, black version, MD letters and Cymothoe (butterfly) in an arrowed circle'} 
                   altDark={'Logo of MD, white version, MD letters and Cymothoe (butterfly) in an arrowed circle'}
-                  className='h-auto mobiles:h-4'
+                  className='h-auto'
         />
       </a>
       <Navbar />
