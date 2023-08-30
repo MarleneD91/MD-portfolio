2f1823e4c78d0e1355fd507b825246d843d75c29
Marlène D <marlene.a.diaz@gmail.com>
2023-08-30 10:18:10 +0200

    add CV (pdf) and download on click

--- components/aboutme/CV.jsx
+++ components/aboutme/CV.jsx
@@ -5,7 +5,7 @@ const CV = () => {
   return (
     <div className='flex flex-col items-center mb-12 mt-5'>
         <h2 className='font-sansita font-bold text-5xl mb-4 text-center mobile-sm:text-4xl'>Mon CV.</h2>
-        <a href="CVofMD"><FaFilePdf className='text-7xl mobile-sm:text-6xl'/></a>
+        <a href="/CV/DIAZM-CV-DevWeb.pdf" download><FaFilePdf className='text-7xl mobile-sm:text-6xl'/></a>
     </div>
   )
 }
