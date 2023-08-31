1a849d08e13cb393c1e9575bfe7860749c22f821
Marlène D <marlene.a.diaz@gmail.com>
2023-08-31 08:03:29 +0200

    ReferenceError: window is not defined -  on Vercel

--- components/common/ThemeSwitcher.jsx
+++ components/common/ThemeSwitcher.jsx
@@ -1,13 +1,13 @@
 "use client"
 
-import React from 'react'
-
 import { useTheme } from 'next-themes'
 
 import { FaMoon, FaSun } from 'react-icons/fa'
 
 const ThemeSwitcher = () => {
+  if (typeof window !== "undefined"){
   const {theme, setTheme} = useTheme()
+  
   const screenSize = window.innerWidth
   return (
     <div  className='fixed bottom-12 right-4 w-[85px] h-10 rounded-[35px] bg-switcher-sun-bg dark:bg-switcher-moon-bg z-0 mobiles:top-3 mobiles:right-3 mobiles:w-11 mobiles:z-50 mobiles:h-11' onClick={screenSize < 581 ? ()=> setTheme( theme === "dark"? "light": "dark" ) : null }>
@@ -21,7 +21,21 @@ const ThemeSwitcher = () => {
         
       </div>
     </div>
-  )
+  )} else {
+    const {theme, setTheme} = useTheme()
+    return (
+      <div  className='fixed bottom-12 right-4 w-[85px] h-10 rounded-[35px] bg-switcher-sun-bg dark:bg-switcher-moon-bg z-0 mobiles:top-3 mobiles:right-3 mobiles:w-11 mobiles:z-50 mobiles:h-11' >
+        <div className='flex flex-row justify-between dark:flex-row-reverse mobiles:flex-none mobiles:p-[6px]'>
+          <FaSun className='dark:hidden place-self-center ml-2 text-3xl text-sun mobiles:hidden'/>
+          <FaMoon className='hidden dark:block place-self-center mr-2 text-3xl text-moon mobiles:hidden'/>
+       
+          <button onClick={()=> setTheme( theme === "dark"? "light": "dark" )} className='rounded-full h-10 w-10 bg-black dark:bg-switcher-button-white z-10 mobiles:hidden'></button>
+     
+          
+        </div>
+      </div>
+    )
+  }
 }
 
 export default ThemeSwitcher
