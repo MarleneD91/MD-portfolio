82eb17ffed1ce781054e184da4e509e47fb0cc00
MarleneD91 <118916540+MarleneD91@users.noreply.github.com>
2023-09-22 20:01:25 +0200

    try head next component

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -6,6 +6,9 @@ import Footer from '@/components/common/Footer/Footer'
 import HomeAnimatedText from '@/components/home/HomeAnimatedText'
 import ThemeImg from '@/components/common/ThemeImg'
 
+//head
+import Head from 'next/head'
+
 //Illustrations
 import Cactus from '@/public/images/Cactus.webp'
 import Magnolia from '@/public/images/Magnolia.webp'
@@ -18,26 +21,31 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <div className='flex flex-col h-screen justify-between'>
-      <Header/>
-        <main className="w-screen" >
-          <div className='flex flex-col items-center justify-between'>
-            <HomeAnimatedText/>
-            <div className='group relative m-5 flex justify-center'>
-              <ThemeImg  LightImage={Cactus} 
-                            DarkImage={Magnolia} 
-                            height={320} width={320} 
-                            altLight={"Cactus in a pot illustration for the plant-themed portfolio, made by MD"} 
-                            altDark={"Magnolia flower illustration for the plant-themed portfolio, made by MD"}
-                            sizes="(max-width: 580px) 70vw"
-                            layout = "responsive"
-                            className="h-[320px] w-auto mobiles:h-auto"
-                            />
-              <span className="absolute bottom-0 scale-0 rounded bg-purple-dark p-1 text-[10px] font-roboto text-white group-hover:scale-100">✨ Je réalise aussi des illustrations ✨</span>
+    <>
+      <Head>
+        <meta name="google-site-verification" content="QJLvHDUn9e77IFuIaN60HBmwMIz9ov5lCq0fxPGAlaI" />
+      </Head>
+      <div className='flex flex-col h-screen justify-between'>
+        <Header/>
+          <main className="w-screen" >
+            <div className='flex flex-col items-center justify-between'>
+              <HomeAnimatedText/>
+              <div className='group relative m-5 flex justify-center'>
+                <ThemeImg  LightImage={Cactus} 
+                              DarkImage={Magnolia} 
+                              height={320} width={320} 
+                              altLight={"Cactus in a pot illustration for the plant-themed portfolio, made by MD"} 
+                              altDark={"Magnolia flower illustration for the plant-themed portfolio, made by MD"}
+                              sizes="(max-width: 580px) 70vw"
+                              layout = "responsive"
+                              className="h-[320px] w-auto mobiles:h-auto"
+                              />
+                <span className="absolute bottom-0 scale-0 rounded bg-purple-dark p-1 text-[10px] font-roboto text-white group-hover:scale-100">✨ Je réalise aussi des illustrations ✨</span>
+              </div>
             </div>
-          </div>
-        </main>
-      <Footer/>
-    </div>
+          </main>
+        <Footer/>
+      </div>
+    </>
   )
 }
