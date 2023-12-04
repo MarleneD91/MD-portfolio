9cff4fa5afeba2187167b3e6213d21c3279bbe73
MarleneD91 <118916540+MarleneD91@users.noreply.github.com>
2023-12-04 08:04:28 +0100

    Update WhoAmI.jsx
    Faster text writing will be better. Test number 1.

--- components/aboutme/WhoAmI.jsx
+++ components/aboutme/WhoAmI.jsx
@@ -17,7 +17,7 @@ const WhoAmI = () => {
                Vous trouverez ici mon parcours et les compétences que j’ai pu acquérir.
                 N’hésitez pas à me contacter pour plus d’informations ! `]}
                 wrapper='span'
-                speed={50}
+                speed={75}
             />
         </div>
         <div className='relative group -z-10'>
